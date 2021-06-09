9a9fec43ad0e2787857ac21f4c395c26c09b2f71
段守志 <shouzhi.duan@nvxclouds.com>
2021-06-09 14:51:34 +0800

    fix code

---
+++ src/main/java/com/kafka/producer/controller/MyTestController.java
@@ -0,0 +1,15 @@
+package com.kafka.producer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Description:
+ */
+@RestController
+@RequestMapping("/test")
+public class MyTestController {
+
+
+}
