a2105e5b3348dab5c90ee6dd0f63df46887804af
段守志 <shouzhi.duan@nvxclouds.com>
2021-06-11 14:38:32 +0800

    fix code

--- src/main/java/com/kafka/producer/controller/MyTestController.java
+++ src/main/java/com/kafka/producer/controller/MyTestController.java
@@ -1,7 +1,14 @@
 package com.kafka.producer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @Auther: ShouZhi@Duan
@@ -11,5 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class MyTestController {
 
+    @GetMapping("/addCookie")
+    public void addCokie(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = new Cookie("name", "dsz");
+        Cookie cookie1 = new Cookie("name1", "dsz1");
+        cookie1.setMaxAge(30);
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+        response.addCookie(cookie1);
+        response.getWriter().print(cookies);
+     }
+
+    @GetMapping("/session")
+    public void session(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        System.err.println("SessionId = " + session.getId());
+    }
 
 }
