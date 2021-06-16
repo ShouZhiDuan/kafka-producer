471719b55680c5325206de7cf941deb27adc7554
段守志 <shouzhi.duan@nvxclouds.com>
2021-06-16 17:55:27 +0800

    fix code

---
+++ src/main/java/com/kafka/producer/DTO/DataDTO.java
@@ -0,0 +1,13 @@
+package com.kafka.producer.DTO;
+
+import lombok.Data;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Description:
+ */
+@Data
+public class DataDTO {
+    private String name;
+    private Integer age;
+}
--- src/main/java/com/kafka/producer/controller/MyTestController.java
+++ src/main/java/com/kafka/producer/controller/MyTestController.java
@@ -1,19 +1,25 @@
 package com.kafka.producer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import com.kafka.producer.DTO.DataDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 /**
  * @Auther: ShouZhi@Duan
  * @Description:
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class MyTestController {
@@ -31,9 +37,79 @@ public class MyTestController {
      }
 
     @GetMapping("/session")
-    public void session(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void session(HttpServletRequest request) throws IOException {
         HttpSession session = request.getSession();
         System.err.println("SessionId = " + session.getId());
+    }
+
+//    @PostMapping(value = "/body",produces = "")
+//    public Object testMsg(@RequestBody DataDTO dto, HttpServletRequest request){
+//        log.info(JSON.toJSONString(dto));
+//        StringBuilder sb = new StringBuilder();
+//        InputStream inputStream = null;
+//        BufferedReader reader = null;
+//        try {
+//            inputStream = request.getInputStream();
+//            reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+//            String line = "";
+//            while ((line = reader.readLine()) != null) {
+//                sb.append(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (inputStream != null) {
+//                try {
+//                    inputStream.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        log.info("BODY1：" + sb.toString().trim());
+//        return dto;
+//    }
+
+    @GetMapping("/get")
+    public Object testMsg(String name, HttpServletRequest request) {
+        log.info(name);
+        StringBuilder sb = new StringBuilder();
+        InputStream inputStream = null;
+        BufferedReader reader = null;
+        try {
+            inputStream = request.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        log.info("BODY2：" + sb.toString().trim());
+        return sb.toString().trim();
     }
 
 }
