f31a7eaac2a9aad19519ed8f15842b70c9be8ab0
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-02 11:12:55 +0800

    fix code

--- src/main/java/com/kafka/producer/task/KafkaTask.java
+++ src/main/java/com/kafka/producer/task/KafkaTask.java
@@ -1,30 +1,17 @@
 package com.kafka.producer.task;
 
-import com.alibaba.fastjson.JSON;
 import com.kafka.producer.DTO.DataDTO;
 import com.kafka.producer.producer.MsgProducerListener;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * @Auther: ShouZhi@Duan
